eac0cafdb7dc8a5e76217732c79b189b9f9b0ef9
Saswat Kar <154300345+Saswat20@users.noreply.github.com>
2026-01-02 17:54:19 +0530

    Update TransferServlet.java

--- TransferServlet.java
+++ TransferServlet.java
@@ -1,27 +1,33 @@
-
 package com.banking.controller;
+
 import com.banking.dao.UserDAO;
 import com.banking.model.User;
-import javax.servlet.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import java.io.*;
 
+@WebServlet("/TransferServlet")
 public class TransferServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-
-        HttpSession session = req.getSession();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
-
-        String toUser = req.getParameter("toUser");
-        double amount = Double.parseDouble(req.getParameter("amount"));
-
-        try {
-            UserDAO dao = new UserDAO();
-            dao.transfer(user.getUsername(), toUser, amount);
-            res.sendRedirect("dashboard.jsp");
-        } catch(Exception e) {
-            e.printStackTrace();
+        
+        if (user == null) { response.sendRedirect("index.jsp"); return; }
+        
+        String receiver = request.getParameter("receiver");
+        double amount = Double.parseDouble(request.getParameter("amount"));
+        
+        UserDAO dao = new UserDAO();
+        boolean success = dao.transferFunds(user.getId(), receiver, amount);
+        
+        if (success) {
+            // Update session balance
+            user = new User(user.getId(), user.getUsername(), dao.getBalance(user.getId()));
+            session.setAttribute("user", user);
+            response.sendRedirect("dashboard.jsp?msg=Transfer Successful");
+        } else {
+            response.sendRedirect("dashboard.jsp?error=Transfer Failed");
         }
     }
 }
